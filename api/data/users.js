4bb9946b369b2dc3af43505a4bf8f6f78fe7f654
pwFoo <pwire.foo@gmail.com>
2017-02-08 20:51:10 +0100

    module converted to jqSlim plugin.

--- api/data/users.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-exports = module.exports = {
-  joao: { name: 'João da Silva', age: 30 },
-  maria: { name: 'Maria Firmina', age: 26 },
-  paulo: { name: 'Paulo Torres', age: 25 },
-  lazy: { name: 'Lazy user', age: 0 }
-}
